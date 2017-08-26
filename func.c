3aca1cc0df40b50f633e6068a9a84a56c03a4979
Har <Har@MacBook-Pro.local>
2017-08-26 14:20:22 +1000

    first commit

---
+++ func.c
@@ -0,0 +1,9 @@
+//
+//  func.c
+//  2003proj1
+//
+//  Created by Har on 26/8/17.
+//  Copyright © 2017 Hardaya Singh. All rights reserved.
+//
+
+#include "func.h"
---
+++ func.h
@@ -0,0 +1,14 @@
+//
+//  func.h
+//  2003proj1
+//
+//  Created by Har on 26/8/17.
+//  Copyright © 2017 Hardaya Singh. All rights reserved.
+//
+
+#ifndef func_h
+#define func_h
+
+#include <stdio.h>
+
+#endif /* func_h */
